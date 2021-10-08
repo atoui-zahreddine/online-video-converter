34b5060b612a23a96775bfda6334683165f0e1ac
zahreddine <zahreddine.atoui@issatso.u-sousse.com>
2021-10-08 15:24:33 +0100

    fix : fix remote baseURL

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { baseUrl: process.env.URL });
 });
 
 module.exports = router;
